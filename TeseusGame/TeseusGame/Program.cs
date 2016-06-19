4fd051b5dc07b50984808c322ea9bad86a87499e
bozhikovstanislav <bozhikov.stanislav@gmail.com>
2016-06-19 15:25:57 +0300

    Test

--- TeseusGame/TeseusGame/Program.cs
+++ TeseusGame/TeseusGame/Program.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TeseusGame
+﻿namespace TeseusGame
 {
+    using  System;
+
     class Program
     {
+        
         static void Main(string[] args)
         {
+
         }
     }
 }
