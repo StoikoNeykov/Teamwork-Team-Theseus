bbf814e205bee7ac833a221ad82936bace637a6c
StoikoNeykov <stoikoneykov@gmail.com>
2016-06-19 17:05:12 +0300

    simple method for test topscore logic

--- TeseusGame/TeseusGame/StartUp.cs
+++ TeseusGame/TeseusGame/StartUp.cs
@@ -1,12 +1,34 @@
 ﻿namespace TeseusGame
 {
+    using System;
     using System.Windows.Forms;
+
+    using TopScores;
 
     public static class StartUp
     {
         public static void Main()
         {
-            
+            TestTopScores();
         }
+
+        private static void TestTopScores()
+        {
+            var result = TopScores.Show();
+            Console.WriteLine(result);
+            PrintLine();
+
+            TopScores.CheckScore("Pesho", 1000);
+            TopScores.CheckScore("Ivan", 50);
+            TopScores.CheckScore("Gosho", 3000);
+            result = TopScores.Show();
+            Console.WriteLine(result);
+
+            TopScores.Clear();
+            result = TopScores.Show();
+            Console.WriteLine(result);
+        }
+
+        private static void PrintLine() => Console.WriteLine(new string('-', 40));
     }
 }
