4ed8bde08017a8be04634d565fe86cf346c7ed20
StoikoNeykov <stoikoneykov@gmail.com>
2016-06-19 15:57:59 +0300

    reposition starting form

--- TeseusGame/TeseusMainGame/TeseusMainForm.Designer.cs
+++ TeseusGame/TeseusMainGame/TeseusMainForm.Designer.cs
@@ -39,12 +39,11 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
             this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(66, 4);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(37, 31);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(384, 362);
+            this.pictureBox1.Size = new System.Drawing.Size(340, 149);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
@@ -52,10 +51,9 @@
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Segoe Print", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(87, 671);
-            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Location = new System.Drawing.Point(71, 430);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(363, 69);
+            this.button4.Size = new System.Drawing.Size(272, 56);
             this.button4.TabIndex = 9;
             this.button4.Text = "Credits";
             this.button4.UseVisualStyleBackColor = true;
@@ -63,10 +61,9 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Segoe Print", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(87, 593);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Location = new System.Drawing.Point(71, 358);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(363, 70);
+            this.button3.Size = new System.Drawing.Size(272, 57);
             this.button3.TabIndex = 8;
             this.button3.Text = "Top Scores";
             this.button3.UseVisualStyleBackColor = true;
@@ -74,10 +71,9 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Segoe Print", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(87, 515);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(71, 286);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(363, 70);
+            this.button2.Size = new System.Drawing.Size(272, 57);
             this.button2.TabIndex = 7;
             this.button2.Text = "How To Play";
             this.button2.UseVisualStyleBackColor = true;
@@ -85,26 +81,26 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Segoe Print", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(87, 426);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(71, 214);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(363, 70);
+            this.button1.Size = new System.Drawing.Size(272, 57);
             this.button1.TabIndex = 6;
             this.button1.Text = "Play";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // TeseusMainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(521, 741);
+            this.ClientSize = new System.Drawing.Size(412, 522);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "TeseusMainForm";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
