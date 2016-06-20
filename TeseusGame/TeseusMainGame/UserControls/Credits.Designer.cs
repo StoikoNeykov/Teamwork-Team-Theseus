77d6a1b2bc317efb2c3765efa0f651d424155543
StoikoNeykov <stoikoneykov@gmail.com>
2016-06-20 10:41:57 +0300

    Credits added (this time for real! )

---
+++ TeseusGame/TeseusMainGame/UserControls/Credits.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace TheseusMainGame.UserControls
+{
+    partial class Credits
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Credits));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Close = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe Script", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 403);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(275, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "7. Name 7";
+            // 
+            // Close
+            // 
+            this.Close.Font = new System.Drawing.Font("Segoe Print", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Close.Location = new System.Drawing.Point(67, 450);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(272, 60);
+            this.Close.TabIndex = 12;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe Script", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(64, 301);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(275, 34);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "4. Name 4";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Segoe Script", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(64, 199);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(275, 34);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "1. Name 1";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Segoe Script", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(64, 267);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(275, 34);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "3. Name 3";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Segoe Script", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(64, 233);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(275, 34);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "2. Name 2";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Segoe Script", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(64, 335);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(275, 34);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "5. Name 5";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Segoe Script", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(64, 369);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(275, 34);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "6. Name 6";
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Segoe Script", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(61, 154);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(275, 45);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Credits";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(59, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(280, 124);
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Credits
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(412, 522);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.label1);
+            this.Name = "Credits";
+            this.Text = "Credits";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ TeseusGame/TeseusMainGame/UserControls/Credits.cs
@@ -0,0 +1,18 @@
+﻿namespace TheseusMainGame.UserControls
+{
+    using System;
+    using System.Windows.Forms;
+
+    public partial class Credits : Form
+    {
+        public Credits()
+        {
+            InitializeComponent();
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
